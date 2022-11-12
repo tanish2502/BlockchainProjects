81052a7a42a6a89f0e3e43047b5ab05bcc1cc47f
tanish2502 <100233384+tanish2502@users.noreply.github.com>
2022-11-12 23:44:11 +0530

    Update ERC20.sol

--- SupplyChainDapp/contracts/tokens/ERC20.sol
+++ SupplyChainDapp/contracts/tokens/ERC20.sol
@@ -5,6 +5,9 @@ import "./IERC20.sol";
 
 contract ERC20 is IERC20{
 
+    string public constant name = "AltCoin";
+    string public constant coin = "AC";
+    uint256 public constant decimal = 18;
     uint256 public constant _totalSupply = 1000;
     /*
     below mapping:
@@ -13,6 +16,10 @@ contract ERC20 is IERC20{
     mapping(address => mapping(address => uint256)) public allowed;
 
     mapping(address => uint256) public _balance;
+
+    constructor(){
+        _balance[msg.sender] = _totalSupply;
+    }
 
     function totalSupply() public override pure returns (uint256)
     {
@@ -24,17 +31,33 @@ contract ERC20 is IERC20{
         return address(this).balance;
     }
 
-    function allowance(address _owner, address _spender) public override returns (uint256)
+    function allowance(address _owner, address _delegate) public override view returns (uint256)
     {
-        require()
+        return allowed[_owner][_delegate];
     }
     
-    function transfer(address _recipient, uint256 _amount) external returns (bool)
+    function transfer(address _recipient, uint256 _amount) public override returns (bool)
     {
-        require()
+        require(_balance[msg.sender] >= _amount);
+        _balance[msg.sender] -= _amount;
+        _balance[_recipient] += _amount;
+        emit Transfer(msg.sender, _recipient, _amount);
+        return true;
     }
 
-    function approve(address spender, uint256 numTokens) external returns (bool);
+    function approve(address _delegate, uint256 _numTokens) public override returns (bool)
+    {
+        allowed[msg.sender][_delegate] = _numTokens;
+        emit Approval(_delegate, msg.sender, _numTokens);
+        return true;
+    }
 
-    function transferFrom(address spender, address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address _spender, address _recipient, uint256 _amount) public override returns (bool)
+    {
+        require(_balance[_spender] >= _amount);
+        _balance[_spender] -= _amount;
+        _balance[_recipient] += _amount;
+        emit Transfer(_spender, _recipient, _amount);
+        return true;
+    }
 }
