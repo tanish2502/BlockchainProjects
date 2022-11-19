bcdfb952652dce45fcc4907df430115d3c06513b
tanish2502 <100233384+tanish2502@users.noreply.github.com>
2022-11-19 23:52:02 +0530

    Update todo.sol

--- todoDapp/contract/todo.sol
+++ todoDapp/contract/todo.sol
@@ -2,16 +2,30 @@
 pragma solidity >= 0.7.0 <0.9.0;
 
 contract Task{
-    event AddTask(uint16 taskId, string recipient);
-    event DeleteTask(uint16 taskId, bool isTaskDelete);
+    event AddTask(uint taskId, address recipient);
+    event DeleteTask(uint taskId, bool isTaskDelete);
 
     struct task{
-        uint16 taskId;
-        string taskTest;
+        uint taskId;
+        string taskText;
         bool isTaskDeleted;
     }
 
     //array of all the tasks
-    task[] tasks;
+    task[] private tasks;
+    mapping(uint => address) taskOfOwner;
+
+    function addTask(string memory _taskText, bool _isDeleted) external
+    {
+        uint taskId = tasks.length;
+        tasks.push(task(taskId, _taskText, _isDeleted));
+        taskOfOwner[taskId] = msg.sender;
+        emit AddTask(taskId, msg.sender);
+    }
+
+    function getTask(uint _taskId) external view returns(uint, string memory, bool)
+    {
+        return (tasks[_taskId].taskId, tasks[_taskId].taskText, tasks[_taskId].isTaskDeleted);
+    }
 
 }
